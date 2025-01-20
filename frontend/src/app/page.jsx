d359abfd244eed1704e01208f39df754d3c25907
yasserft <yasser.elfetouh@etu.uae.ac.ma>
2025-01-20 23:20:56 +0100

    commit test

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className='flex items-center justify-center h-screen bg-gray-50'>
-      <h1 className='text-4xl font-bold'>Welcome to My Next.js App!</h1>
+      <h1 className='text-4xl font-bold'>Welcome to My ReactJs App!</h1>
     </div>
   );
 }
